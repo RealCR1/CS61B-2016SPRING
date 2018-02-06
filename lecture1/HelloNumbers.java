6b20989054ff428c2445002fdf396cae198a4347
RealCR1 <Wz892477341@outlook.com>
2018-02-06 17:40:27 +0800

    Add some source code of chapter 1.1 in reading textbook.

--- lecture1/HelloNumbers.java
+++ lecture1/HelloNumbers.java
@@ -1,10 +1,12 @@
-
 public class HelloNumbers {
 	public static void main(String[] args){
-		int x;
-		x = 0;
+		int x = 0;
+		int sum = 0;
 		while (x < 10){
-			System.out.println(x);
+			sum = sum + x;
+			System.out.println(sum);
+		    x = x + 1;
 		}
+			
 	}	
 }
---
+++ lecture1/LargerDemo.java
@@ -0,0 +1,17 @@
+
+public class LargerDemo {
+	/** Return the larger number of x and y. */
+	public static int larger(int x, int y){
+		if (x > y){
+			return x; 
+		}
+		return y;
+
+	}
+
+	public static void main(String[] args){
+
+		System.out.println(larger(-5, 10));
+		
+	}	
+}
