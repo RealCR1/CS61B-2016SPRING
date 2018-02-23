fbab141405872efe212e1e98cc9379ba928a33f3
RealCR1 <Wz892477341@outlook.com>
2018-02-23 10:17:08 +0800

    Adding main method and readRadius method.

--- skeleton-sp17-master/proj0/NBody.java
+++ skeleton-sp17-master/proj0/NBody.java
@@ -1,3 +1,25 @@
 public class NBody {
+	public static double readRadius(String filename)
+	{
+		In in = new In(filename);
+		in.readLine();
+		return in.readDouble();
+	}
+
+
+
+
+	public static void main(String[] args) {
+		double T = double.parseDouble(args[0]);
+		double dt = double.parseDouble(args[1]);
+		String filename = args[2];
+
+		Planet[] plants = readPlanets(filename);
+		double radius = readRadius(filename);
+
+
+
+	}
+
 
 }
--- skeleton-sp17-master/proj0/Planet.java
+++ skeleton-sp17-master/proj0/Planet.java
@@ -122,4 +122,6 @@ public class Planet {
 	{
 		return f / mass;
 	}
+
+
 }
