4f455e396e4a3cde3e3727c572d9725daca6f085
RealCR1 <Wz892477341@outlook.com>
2018-02-26 23:14:03 +0800

    Adding something.

--- lecture/lecture4/SLList.java
+++ lecture/lecture4/SLList.java
@@ -2,20 +2,39 @@
  * Hides the terrible truth of nakeness.
  */
 public class SLList {
-	public IntNode first;
+	
 
+/** A nested class. */
+
+	public class IntNode {
+	
+		public int item;
+		public IntNode next;
+
+		public IntNode(int i, IntNode n){
+			item = i;
+			next = n;
+	}
+
+
+	public IntNode first;
 	public SLList(int x){
 		first = new IntNode(x, null);
 	}
-	
+
+
+	/** Adds x to the front of the list. */
+	public void addFirst(int x){
+		 first = new IntNode(x, first);
+	}
+
+	/** Return the first element in the list. */
+	public int getFirst(){
+		return first.item;
+	}
+}
 
 	public static void main(String[] args){
 		SLList L = new SLList(10); //There is no need to specify null.
 	}	
 
-	/** Adds x to the front of the list. */
-	public void addFirst(int x){
-		 first = new IntNode(x, first);
-
-	}
-}
