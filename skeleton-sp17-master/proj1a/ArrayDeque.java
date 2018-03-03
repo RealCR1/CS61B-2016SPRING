507e586c0d249910ab9793351628173307ab43a6
RealCR1 <Wz892477341@outlook.com>
2018-03-03 22:24:36 +0800

    ArrayDeque has been done.

--- skeleton-sp17-master/proj1a/ArrayDeque.java
+++ skeleton-sp17-master/proj1a/ArrayDeque.java
@@ -24,7 +24,7 @@ public class ArrayDeque<Item> {
 
     //Try to create an ArrayDeque class constructor which has zero argument.
     //This will create an empty Deque.
-    public ArrayDeque(){
+    public ArrayDeque() {
         arr = (Item[]) new Object[INITIAL_CAPACITY];
 
     }
@@ -32,34 +32,33 @@ public class ArrayDeque<Item> {
     //This method will add a item as the first element in ArrayDeque.
     //The front pointer should loop back around to the end of the array.
     //So a circular array is a good idea.
-    public void addFirst(Item item){
-        if(item == null){
+    public void addFirst(Item item) {
+        if (item == null) {
             throw new NullPointerException();
         }
         arr[nextFirst] = item;
-        if(this.nextFirst == 0){
+        if (this.nextFirst == 0) {
             nextFirst = this.arr.length - 1;
-        }
-        else{
+        } else {
             this.nextFirst -= 1;
         }
 
         size += 1;
-        if(size == arr.length){
+        if (size == arr.length) {
             this.resize(arr.length * 2);
         }
     }
 
 
     //Adds an item to the back of the Deque.
-    public void addLast(Item item){
-        if(item == null){
+    public void addLast(Item item) {
+        if (item == null) {
             throw new NullPointerException();
         }
         arr[nextLast] = item;
         nextLast = (nextLast + 1) % (this.arr.length - 1);
         size += 1;
-        if(size == arr.length){
+        if (size == arr.length) {
             this.resize(arr.length * 2);
         }
 
@@ -67,11 +66,11 @@ public class ArrayDeque<Item> {
 
 
     //Implementation of a helper method resize().
-    public void resize(int full_number){
+    public void resize(int full_number) {
         int n = arr.length;
         Item[] new_arr = (Item[]) new Object[full_number];
         int pointer = 0;
-        while(arr[pointer] == null){
+        while (arr[pointer] == null) {
             pointer += 1;
         }
         System.arraycopy(this.arr, pointer, new_arr, 0, size);
@@ -81,13 +80,13 @@ public class ArrayDeque<Item> {
     }
 
     //Return the size of this ArrayDeque.
-    public int size(){
+    public int size() {
         return size;
     }
 
 
     //Return True if ArrayDeque isn't empty.
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size == 0;
     }
 
@@ -96,51 +95,79 @@ public class ArrayDeque<Item> {
 
 
     //Removes and returns the first item of the Deque.
-    public Item removeFirst(){
-        if(size = 0){
+    public Item removeFirst() {
+        if (size == 0) {
             return null;
         }
         Item item;
-        if(nextFirst == arr.length - 1){
-            item = (Item)arr[0];
+        if (nextFirst == arr.length - 1) {
+            item = (Item) arr[0];
             arr[0] = null;
             nextFirst = 0;
-        }
-        else{
+        } else {
             item = (Item) arr[nextFirst + 1];
             arr[nextFirst + 1] = null;
             nextFirst += 1;
         }
         size -= 1;
+
+        //For memory efficiency, resize the length of array is compulsory.
+        //From other's solution.
+        if (size == arr.length / 4 && size == 0 && arr.length >= 16) {
+            this.resize(arr.length / 2);
+        }
+
         return item;
     }
 
 
     //Removes and returns the item at the back of the Deque.
-    public Item removeLast(){
-        if(size == 0){
+    public Item removeLast() {
+        if (size == 0) {
             return null;
         }
         Item item;
-        if(nextLast == 0){
+        if (nextLast == 0) {
             item = (Item) arr[arr.length - 1];
             arr[arr.length - 1] = null;
             nextLast = arr.length - 1;
-        }
-        else{
+        } else {
             item = (Item) arr[nextLast - 1];
             arr[nextLast - 1] = null;
             nextLast -= 1;
         }
         size -= 1;
+
+        //For memory efficiency, resize the length of array is compulsory.
+        //From other's solution.
+        if (size == arr.length / 4 && size == 0 && arr.length >= 16) {
+            this.resize(arr.length / 2);
+        }
+
         return item;
     }
 
+    //Get the item at the special index.
+    public Item get(int index) {
+        int indexIntArray;
+        int starter = nextFirst + 1;
+        if (index > this.size - 1) {
+            return null;
+        }
+        else if (nextFirst == arr.length - 1) {
+                indexIntArray = index;
+            }
+        else{
+            if (index < arr.length - 1 - starter) {
+                indexIntArray = (starter + index) % (arr.length - 1);
+            } else if (index == arr.length - 1 - starter) {
+                indexIntArray = arr.length - 1;
+            } else {
+                indexIntArray = (starter + index) % (arr.length - 1) - 1;
+            }
+        }
 
+        return arr[indexIntArray];
+        }
+    }
 
-
-
-
-
-
-}
