aa42b6ee77c1a7b6a698b065b3ded94bcb9453f1
RealCR1 <Wz892477341@outlook.com>
2018-03-04 20:39:56 +0800

    Finished the unimplemented method.

--- lectureCode-sp17-master/lists4/DIY/AList.java
+++ lectureCode-sp17-master/lists4/DIY/AList.java
@@ -4,20 +4,30 @@
 
 public class AList {
     /** Creates an empty list. */
+
+    private int[] item;
+    private int size;
+
     public AList() {
+        item = new int[100];
+        size = 0;
     }
 
     /** Inserts X into the back of the list. */
     public void addLast(int x) {
+        item[size] = x;
+        size += 1;
     }
 
     /** Returns the item from the back of the list. */
     public int getLast() {
-        return 0;        
+        return item[size - 1];
     }
+
+
     /** Gets the ith item in the list (0 is the front). */
     public int get(int i) {
-        return 0;        
+        return item[i];
     }
 
     /** Returns the number of items in the list. */
@@ -28,6 +38,18 @@ public class AList {
     /** Deletes item from back of the list and
       * returns deleted item. */
     public int removeLast() {
-        return 0;
+
+/*  There is no need to destroy that item.
+    We can use the method that we have defined.
+
+        int item_removed = item[size - 1];
+        item[size - 1] = 0;
+        size -= 1;
+        return item_removed;
+*/
+        int x = getLast();
+        size -= 1;
+        return x;
+
     }
 } 
