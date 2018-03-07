0d3fb3682ec4449410ae9993a5ecfd4ae8ffeaf5
RealCR1 <Wz892477341@outlook.com>
2018-03-07 09:45:00 +0800

    Rewrite the Sort.java to improve the understanding.

--- skeleton-sp17-master/lab3/Flik/Sort.java
+++ skeleton-sp17-master/lab3/Flik/Sort.java
@@ -15,24 +15,25 @@ public class Sort {
 	    sort(x, start + 1);
     }
 
-	/** Swap item a with b. */
-	public static void swap(String[] x, int a, int b) {
-	    String temp = x[a];
-	    x[a] = x[b];
-	    x[b] = temp;
-    }
-    
 
 	/** Return the index of the smallest String in x, starting at start. */
 	public static int findSmallest(String[] x, int start) {
         int smallestIndex = start;
-        for (int i = start; i < x.length; i += 1) {
-            int cmp = x[i].compareTo(x[smallestIndex]);
-            // from the internet, if x[i] < x[smallestIndex], cmp will be -1.
-            if (cmp < 0) {
+        for (int i = start; i < x.length; i++){
+            int compResult = x[i].compareTo(x[smallestIndex]);
+            if(compResult < 0){
                 smallestIndex = i;
             }
         }
         return smallestIndex;
+    }
+
+
+
+    /** Swap item a with b. */
+    public static void swap(String[] x, int a, int b) {
+        String temp = x[a];
+        x[a] = x[b];
+        x[b] = temp;
     }
 }
