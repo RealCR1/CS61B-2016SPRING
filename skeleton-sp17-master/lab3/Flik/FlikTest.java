fb8fc449c7e684d55780fd9d150de2cb39e8708c
RealCR1 <Wz892477341@outlook.com>
2018-03-10 21:49:56 +0800

    Using the assetTrue() or assertFalse() in convenient.

--- skeleton-sp17-master/lab3/Flik/FlikTest.java
+++ skeleton-sp17-master/lab3/Flik/FlikTest.java
@@ -8,9 +8,9 @@ import org.junit.Test;
 public class FlikTest {
     @Test
     public void testIsSameNumber() {
-        assertEquals(true, Flik.isSameNumber(5, 5));
-        assertEquals(true, Flik.isSameNumber(0, 0));
-        assertEquals(false, Flik.isSameNumber(5, -5));
+        assertTrue(Flik.isSameNumber(5, 5));
+        assertFalse(Flik.isSameNumber(5, -5));
+        assertTrue(Flik.isSameNumber(0, 0));
     }
 
 }
