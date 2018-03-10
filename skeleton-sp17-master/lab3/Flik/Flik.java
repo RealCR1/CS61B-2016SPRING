0d9f516384d3ace0d99bacdf32a32c73b67978c2
RealCR1 <Wz892477341@outlook.com>
2018-03-10 22:41:17 +0800

    Writing my opinion of this bug.

--- skeleton-sp17-master/lab3/Flik/Flik.java
+++ skeleton-sp17-master/lab3/Flik/Flik.java
@@ -1,6 +1,13 @@
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
+        /*The former code is
+        * return a == b;
+        * You know, the '==' compare the reference to the pointer.
+        * And the equals() compares the true value.
+        * When integer is instantiated in Java, it should has 32 bits.
+        * When it comes to 128, pointers of a and b are different.
+        * */
         return a.equals(b);
     }
 }
