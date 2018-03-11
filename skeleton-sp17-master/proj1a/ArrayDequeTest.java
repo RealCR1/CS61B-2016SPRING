d7077717362c9cc3055c135ae0a8513d76838958
RealCR1 <Wz892477341@outlook.com>
2018-03-11 22:30:14 +0800

    Adding test of Integer.

--- skeleton-sp17-master/proj1a/ArrayDequeTest.java
+++ skeleton-sp17-master/proj1a/ArrayDequeTest.java
@@ -8,6 +8,8 @@ public class ArrayDequeTest {
 
     @Test
     public void testAddRemove() {
+        /* Write test of String.
+        **/
         String[] expected = {"a", "b", "c", "d"};
 
         ArrayDeque<String> stringArr = new ArrayDeque<>();
@@ -23,6 +25,26 @@ public class ArrayDequeTest {
             assertEquals(stringArr.get(i), expected[i]);
         }
 
+
+        /* Write test of integer.
+        */
+        int[] expected = {1, 2, 3, 4, 5};
+
+        ArrayDeque<Integer> intArr = new ArrayDeque<>();
+
+        intArr.addLast(4);
+        intArr.addLast(5);
+        intArr.addFirst(3);
+        intArr.addFirst(2);
+        intArr.addFirst(0);
+        intArr.addLast(6);
+
+        intArr.removeFirst();
+        intArr.removeLast();
+
+        for(int j = 0; j < expected.length; j++) {
+            assertEquals(expected[j], intArr.get(i).intValue());
+        }
 
 
     }
