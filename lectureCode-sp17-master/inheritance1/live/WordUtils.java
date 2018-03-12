8d3e6d55aaa32795bc49c2577a1870ff039ccfb2
RealCR1 <Wz892477341@outlook.com>
2018-03-12 18:48:59 +0800

    Using the overloading method for suiting two conditions: AList and SLList.

--- lectureCode-sp17-master/inheritance1/live/WordUtils.java
+++ lectureCode-sp17-master/inheritance1/live/WordUtils.java
@@ -1,6 +1,6 @@
 public class WordUtils {
    /** Returns the length of the longest word. */
-   public static String longest(List61B<String> list) {
+   public static String longest(AList<String> list) {
       int maxDex = 0;
       for (int i = 0; i < list.size(); i += 1) {
          String longestString = list.get(maxDex);
@@ -11,6 +11,20 @@ public class WordUtils {
       }
       return list.get(maxDex);
    }
+
+    /* Try to solve the SLList problem.
+    *  Using the overloading method.*/
+    public static String longest(SLList<String> list) {
+        int maxDex = 0;
+        for (int i = 0; i < list.size(); i += 1) {
+            String longestString = list.get(maxDex);
+            String thisString = list.get(i);
+            if (thisString.length() > longestString.length()) {
+                maxDex = i;
+            }
+        }
+        return list.get(maxDex);
+    }
 
     public static void main(String[] args) {
      AList<String> someList = new AList<>();
