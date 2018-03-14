7a72e5dc707a791ba85d8baf60a84b81ab88a5f1
RealCR1 <Wz892477341@outlook.com>
2018-03-14 19:41:09 +0800

    Task I in Phase I has been finished.

--- skeleton-sp17-master/proj1b/TestArrayDeque1B.java
+++ skeleton-sp17-master/proj1b/TestArrayDeque1B.java
@@ -1,7 +1,11 @@
-/** @author Jonathan Wang
- *  3/14/2018
- *  Project 1B Phase I Task I.*/
+/**
+ * @author Jonathan Wang
+ * 3/14/2018
+ * Project 1B Phase I Task I.
+ */
+
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class TestArrayDeque1B {
@@ -16,46 +20,42 @@ public class TestArrayDeque1B {
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
 
-        for (int i = 0; i < 100; i++) {
-            /** For checking at least four methods,
-             *  We need instantiate a integer variable valuing from 0 to 3.
-             */
-            for (int i = 0; i < 50; i++) {
-                int checkNumber = StdRandom.uniform(4);
 
-                switch (checkNumber) {
-                    case 0:
-                        sad.addFirst(i);
-                        ads.addFirst(i);
-                        break;
-                    case 1:
-                        sad.addLast(i);
-                        ads.addLast(i);
-                    case 2:
-                        if (!sad.isEmpty() && ads.size() > 0) {
-                            sad.removeFirst();
-                            ads.removeFirst();
-                        }
-                        break;
-                    case 3:
-                        if (!sad.isEmpty() && ads.size() > 0) {
-                            sad.removeLast();
-                            ads.removeLast();
-                        }
-                        break;
-                    default:
-                        break;
+        /** For checking at least four methods,
+         *  We need instantiate a integer variable valuing from 0 to 3.
+         */
+        for (int i = 0; i < 50; i++) {
+            int checkNumber = StdRandom.uniform(4);
 
-                }
-
+            switch (checkNumber) {
+                case 0:
+                    sad.addFirst(i);
+                    ads.addFirst(i);
+                    break;
+                case 1:
+                    sad.addLast(i);
+                    ads.addLast(i);
+                case 2:
+                    if (!sad.isEmpty() && ads.size() > 0) {
+                        sad.removeFirst();
+                        ads.removeFirst();
+                    }
+                    break;
+                case 3:
+                    if (!sad.isEmpty() && ads.size() > 0) {
+                        sad.removeLast();
+                        ads.removeLast();
+                    }
+                    break;
+                default:
+                    break;
             }
-
-
-
-
         }
 
+        for(int j = 0; j < ads.size(); j++) {
+            assertEquals(sad.get(j), ads.get(j));
+        }
 
-
+        assertEquals(ads.size(), sad.size());
     }
 }
