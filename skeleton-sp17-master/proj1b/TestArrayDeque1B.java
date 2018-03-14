c3feed68484119b95bac247791e9165890094505
RealCR1 <Wz892477341@outlook.com>
2018-03-14 19:36:11 +0800

    Finish most work of this test method.

--- skeleton-sp17-master/proj1b/TestArrayDeque1B.java
+++ skeleton-sp17-master/proj1b/TestArrayDeque1B.java
@@ -5,22 +5,53 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDeque1B {
-    @Test
     /** The name of this test doesn't matter.
      *  This test should randomly call StudentArrayDeque() method,
      *  and ArrayDequeSolution() method until they disagree on an output.
      *
      *  And using the StdRandom() to generate random number.
      *  */
-    public void testCallTwoMethods(int testTime) {
-        for (int i = 0; i < testTime; i++) {
-            double numberBetweenOneAndZero = StdRandom.uniform();
+    @Test
+    public void testCallTwoMethods() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
 
-            if (numberBetweenOneAndZero < 0.5) {
-                testStudentArrayDeque();
-            } else {
-                testArrayDequeSolution();
+        for (int i = 0; i < 100; i++) {
+            /** For checking at least four methods,
+             *  We need instantiate a integer variable valuing from 0 to 3.
+             */
+            for (int i = 0; i < 50; i++) {
+                int checkNumber = StdRandom.uniform(4);
+
+                switch (checkNumber) {
+                    case 0:
+                        sad.addFirst(i);
+                        ads.addFirst(i);
+                        break;
+                    case 1:
+                        sad.addLast(i);
+                        ads.addLast(i);
+                    case 2:
+                        if (!sad.isEmpty() && ads.size() > 0) {
+                            sad.removeFirst();
+                            ads.removeFirst();
+                        }
+                        break;
+                    case 3:
+                        if (!sad.isEmpty() && ads.size() > 0) {
+                            sad.removeLast();
+                            ads.removeLast();
+                        }
+                        break;
+                    default:
+                        break;
+
+                }
+
             }
+
+
+
 
         }
 
