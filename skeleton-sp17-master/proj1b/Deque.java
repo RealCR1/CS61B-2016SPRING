6f8ec52a84d7692543217381554c76b31e2e425e
RealCR1 <wz892477341@outlook.com>
2018-03-15 17:36:11 +0800

    key word 'public' is reduntant.

--- skeleton-sp17-master/proj1b/Deque.java
+++ skeleton-sp17-master/proj1b/Deque.java
@@ -3,22 +3,22 @@ public interface Deque<Item> {
     /**
      * Remove the first item from the ArrayDeque.
      * */
-    public Item removeFirst();
+    Item removeFirst();
 
     /**
      * Remove the last item from the ArrayDeque.
      **/
-    public Item removeLast();
+    Item removeLast();
 
     /**
      * Using recursion way to get items.
      * */
-    public Item getRecursive(int i);
+    Item getRecursive(int i);
     
     /**
      * Print each item of the ArrayDeque.
      */
-    public void printDeque();
+    void printDeque();
     
     
     }
