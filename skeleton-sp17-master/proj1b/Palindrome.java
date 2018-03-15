df19ad2c3200f08e95e0ae285a33f061edddafdb
RealCR1 <wz892477341@outlook.com>
2018-03-15 22:08:55 +0800

    Adding isPalindrome() method.

--- skeleton-sp17-master/proj1b/Palindrome.java
+++ skeleton-sp17-master/proj1b/Palindrome.java
@@ -16,8 +16,29 @@ public class Palindrome {
      *  if the given word is a palindrome and false otherwise.
      *  A Palindrome is defined as a word that is the same whether it is read forwards or backwards.
      *  */
-    public static boolean isPalindrome() {
-        
-        
+    public static boolean isPalindrome(String word) {
+        /** Before using this method to check
+         *  We should change input into Deque.
+         *  */
+        Deque<Character> wordToDeque = wordToDeque(word);
+        return isPalindrome(wordToDeque);
     }
+    
+    /** Define a method using the input format as Deque.*/
+    public static boolean isPalindrome(Deque<Character> deque) {
+        if (deque.size() == 0 || deque.size() == 1) {
+            return true;
+        }
+        
+        char firstItem = deque.removeFirst();
+        char lastItem = deque.removeLast();
+        
+        if (firstItem == lastItem) {
+            return true;
+        }
+        
+        return isPalindrome(deque);
+    }
+    
+    
 }
