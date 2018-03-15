9b7705b479260f2e276c52b0be93bf1e542aa5c2
RealCR1 <wz892477341@outlook.com>
2018-03-15 17:34:59 +0800

    Adding Deque.java as interface.

---
+++ skeleton-sp17-master/proj1b/Deque.java
@@ -0,0 +1,32 @@
+/** Creating a interface.*/
+public interface Deque<Item> {
+    /**
+     * Remove the first item from the ArrayDeque.
+     * */
+    public Item removeFirst();
+
+    /**
+     * Remove the last item from the ArrayDeque.
+     **/
+    public Item removeLast();
+
+    /**
+     * Using recursion way to get items.
+     * */
+    public Item getRecursive(int i);
+    
+    /**
+     * Print each item of the ArrayDeque.
+     */
+    public void printDeque();
+    
+    
+    }
+    
+    
+    
+    
+    
+
+
+ 
