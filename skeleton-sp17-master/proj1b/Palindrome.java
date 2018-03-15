00d32766634e4238be012e874bab88fcf59addd3
RealCR1 <wz892477341@outlook.com>
2018-03-15 21:55:04 +0800

    Writing wordToDeque() method.

--- skeleton-sp17-master/proj1b/Palindrome.java
+++ skeleton-sp17-master/proj1b/Palindrome.java
@@ -4,14 +4,20 @@ public class Palindrome {
      *  appear in the same order as in the word.
      *  */
     public static Deque<Character> wordToDeque(String word) {
-        
+        Deque<Character> wordToDeque = new ArrayDeque<>();
+        for (int i = 0; i <= word.length(); i++) {
+           char character = word.charAt(i);
+           wordToDeque.addLast(character);
+        }
+       return wordToDeque; 
     }
     
     /** The isPalindrome method should return true,
      *  if the given word is a palindrome and false otherwise.
      *  A Palindrome is defined as a word that is the same whether it is read forwards or backwards.
      *  */
-    public static boolean isPalindrome(String word) {
+    public static boolean isPalindrome() {
+        
         
     }
 }
