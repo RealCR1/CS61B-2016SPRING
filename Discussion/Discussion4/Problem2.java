53ec18ace227ad2644b71c56bbb0b33f4a72fcdc
RealCR1 <wz892477341@outlook.com>
2018-03-18 19:55:37 +0800

    Problem 2 in Discussion 4 ep.

---
+++ Discussion/Discussion4/Problem2.java
@@ -0,0 +1,20 @@
+public class Problem2 {
+    /**We need to keep track of rest list after the operation.
+     * Using iteration to do the exchange address of the pointer.
+     * 
+     * Where to start? From the beginning or the last?
+     * If we choose to work from last one,
+     * we couldn't go to the end of the list.
+     * So tackle it from the beginning.
+     * */
+/*    
+    while (A != null) {
+        IntList t1 = A.rest;
+        A.rest = B.rest;
+        B.rest = t1;
+        
+        B = A.rest;
+        A = t1;*/
+    }
+    
+}
