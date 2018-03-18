fc2de2f04d0acd90e0676aa92c0b7158d3169797
RealCR1 <wz892477341@outlook.com>
2018-03-18 13:29:10 +0800

    During the video lecture. Change some code.

--- lectureCode-sp17-master/inheritance4/hertz/DemoCollections.java
+++ lectureCode-sp17-master/inheritance4/hertz/DemoCollections.java
@@ -9,55 +9,57 @@ public class DemoCollections {
 
 	/** Returns a lower case version of the string with
 	  * all characters except letters removed. */
-	public static String cleanString(String s) {
-		return s.toLowerCase().replaceAll("[^a-z]", "");
+    public static String cleanString(String s) {
+        return s.toLowerCase().replaceAll("[^a-z]", "null");
+	    /** [^a-z] to check if any characters except a-z.
+         *  Same to check the A-Z expect a-z in file.
+         *  So the method is to change all characters which isn't a-z into null.
+         *  
+         *  '.*[^a-z].*' won't fail on more than one character.
+         *  */
+        
 	}
 
 	/** Gets a list of all words in the file. */
-	public static List<String> getWords(String inputFilename) {
-		List<String> words = new ArrayList<String>();
-		In in = new In(inputFilename);
-		while (!in.isEmpty()) {
-			String nextWord = cleanString(in.readString());
-			words.add(nextWord);
-		}
-		return words;
-	}
+    public static List<String> getWords(String inputfilename) {
+        List<String> words = new ArrayList<String>();
+        In in = new In(inputfilename);
+        while (!in.isEmpty()) {
+            String nextWord = cleanString(in.readString());
+            words.add(nextWord);
+        }
+        return words;
+    }
 
 	/** Returns the count of the number of unique words in words. */
 	public static int countUniqueWords(List<String> words) {
-		Set<String> wordSet = new HashSet<String>();
-		for (String ithWord : words) {
-			wordSet.add(ithWord);
-		}
-
-		return wordSet.size();
+		Set<String> strWords = new HashSet<String>();
+		for (String ithWord : strWords) {
+		    strWords.add(ithWord);
+        }
+        return strWords.size();
 	}
 
 	/** Returns a map (a.k.a. dictionary) that tracks the count of all specified
 	  * target words in words. */
 	public static Map<String, Integer> collectWordCount(List<String> words, List<String> targets) {
 		Map<String, Integer> counts = new HashMap<String, Integer>();
-		/* make a note that we have seen none of the words. */
 		for (String t : targets) {
-			counts.put(t, 0);
-		}
-
-		for (String s : words) {
-			if (counts.containsKey(s)) {
-				int oldCount = counts.get(s);
-				counts.put(s, oldCount + 1);
-			}
-		}
-		return counts;
+		    counts.put(t, 0);
+        }
+        
+        for (String s : words) {
+		    int oldCount = counts.get(s);
+		    counts.put(s, oldCount + 1);
+        }
+        return counts;
 	}
 
 	public static void main(String[] args) {
 		List<String> w = getWords("libraryOfBabylon.txt");
 		System.out.println(countUniqueWords(w));
-
-
-		List<String> targets = new ArrayList<>();
+		
+		List<String> targets = new ArrayList<String>();
 		targets.add("lottery");
 		targets.add("the");
 		targets.add("babylon");
