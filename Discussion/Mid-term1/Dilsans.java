a7dd19e61bf59a63cb0aaca29d80dcb6ab909cd0
RealCR1 <wz892477341@outlook.com>
2018-03-24 21:20:03 +0800

    Adding Part 4 Sans of mid-term.

--- Discussion/Mid-term1/Dilsans.java
+++ Discussion/Mid-term1/Dilsans.java
@@ -1,15 +1,16 @@
 /** In order to get a better solution video
 	I chose mid-term-1 in Spring 2018 instead of mid-term-1 in Spring 2017.
-	This is Part 4 Sans (b) in spring 2018.
+	This is Part 4 Sans (a) in spring 2018.
 	
 	@author Jonathan Wang.
 	*/
-public static IntList ilsans(IntList x, int y) {
+public static IntList dilsans(IntList x, int y) {
 	if (x == null) {
 		return null;
 	}
+	x.rest = dilsans(x.rest, y);
 	if (x.first == y) {
-		return ilsans(x.rest, y);
+		return x.rest;
 	}
-	return new IntList(x.first, ilsans(x.rest, y));
+	return x;
 }
---
+++ Discussion/Mid-term1/TestSans.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestSans {
+	@Test
+	public void testSans() {
+		// TEST THE ARRAY VERSION OF SANS.
+		int[] x = {1, 2, 3, 4, 5};
+		int y = 3;
+
+		int[] expected = {1, 2, 4, 5};
+		int[] actual = sans(x, y);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testIlsans() {
+		//TEST THE NON-DESTRUCTIVE VERSION OF SANS.
+		IntList x = IntList.of(1, 2, 3, 4, 5);
+		int y = 1;
+		IntList expected = IntList.of(2, 3, 4, 5);
+		IntList actual = Sans.ilsans(x, y);
+		assertEquals(expected, actual);
+		assertEquals(x, actual);
+
+	}
+}
