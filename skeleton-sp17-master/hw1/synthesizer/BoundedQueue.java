02a3190bf1a55cd1dd2a7ed5182af41c02429f5d
RealCR1 <wz892477341@outlook.com>
2018-03-26 23:01:57 +0800

    Adding BoundedQueue.java

---
+++ skeleton-sp17-master/hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,19 @@
+package synthesizer;
+
+public interface BoundedQueue<T> {
+    int capcitity(); //Return size of the buffer.
+    int fillCount(); //return number of items currently in the buffer.
+    void enqueue(T x); // add item x to the end.
+    T dequeue(); // delete and return item from the front.
+    T peek(); // Return (but don't delete) item from the front.
+    
+    // is the buffer empty?
+    default boolean isEmpty() {
+        return (this.fillCount() == 0);
+    }
+    // is the buffer full?
+    default boolean isFull() {
+        return (this.fillCount() == this.capcitity());
+    }
+    
+}
