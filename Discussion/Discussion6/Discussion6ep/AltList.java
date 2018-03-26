928c7f7e2ae278d96faffa853ab38499157c582f
RealCR1 <wz892477341@outlook.com>
2018-03-26 21:47:29 +0800

    Discussion6ep question 2.

---
+++ Discussion/Discussion6/Discussion6ep/AltList.java
@@ -0,0 +1,24 @@
+public class AltList<X, Y> {
+	private X item;
+	private AltList<Y, X> next;
+
+	AltList(X item, AltList<Y, X> next) {
+		this.item = item;
+		this.next = next;
+	}
+
+	AltList<Integer, String> list = 
+		new AltList<Integer, String> (5,
+			new AltList<String, Integer> ("cat",
+				new AltList<Integer, String> (10,
+					new AltList<String, Integer> ("dog", null))));
+
+	public AltList<X, Y> pairsSwapped() {
+		AltList<Y, X> temp = new AltList<Y, X>(next.item, new AltList<X, Y>(item, null));
+		if (next.next != null) {
+			temp.next.next = next.next.pairsSwapped();
+		}
+		return temp;
+
+	}
+}
