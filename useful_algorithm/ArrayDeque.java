23dc6c4fb744d359f5fdc6eb62cfb2a767182084
RealCR1 <wz892477341@outlook.com>
2018-04-07 23:29:56 +0800

    Adding other important array data structure.

---
+++ useful_algorithm/ArrayDeque.java
@@ -0,0 +1,40 @@
+public class ArrayDeque<Item> {
+	private int size = 0;
+	private int nextFirst = 0;
+	private int nextLast = 1;
+
+	private Item[] arr;
+
+	private static final int INITIAL_CAPACITY = 8;
+
+	//Create an empty Deque.
+	public ArrayDeque() {
+		arr = (Item[]) new Object[INITIAL_CAPACITY];
+	}
+
+	//Add first item as the first element in ArrayDeque.
+	//So a circular array is a good idea.
+	public void addFirst(Item item) {
+		if (item == null) {
+			throw new NullPointerException();
+		}
+
+		//Adding the item to the first of the array.
+		arr[nextFirst] = item;
+
+		if (this.nextFirst == 0) {
+			nextFirst = this.length - 1;
+		} else {
+			this.nextFirst -= 1;
+		}
+		size += 1;
+
+		if (size == arr.length) {
+			this.resize(arr.length * 2);
+		}
+
+
+
+	}
+
+}
